d1e7387e9ca9ff7e82b1aeb878297b05d100ee46
Rachel Sena Adorkor <racheladksena@gmail.com>
2025-06-04 18:42:19 -0700

    worked on milestone 3

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,6 @@
+const modal = document.getElementById("playlists");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlists) {
+    document.getElementById("playlist")
+}
