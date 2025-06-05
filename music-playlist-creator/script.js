5f7323a53f0ecf60ee782e708df75acba271cf53
Rachel Sena Adorkor <racheladksena@gmail.com>
2025-06-05 01:48:05 -0700

    finished milestone 4

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,6 +1,39 @@
-const modal = document.getElementById("playlists");
-const span = document.getElementsByClassName("close")[0];
+fetch('data/data.json')
+.then(response => response.json())
+.then(data=> {
+playlistCards(data);
+})
 
-function openModal(playlists) {
-    document.getElementById("playlist")
+.catch(error => {
+    console.error('An error occurred', error)
+});
+
+
+function playlistCards(data) {
+    const parentContainer = document.querySelector(".playlist_cards");
+    parentContainer.innerHTML="";
+
+    if (!data) {
+        parentContainer.innerHTML = `<p>The playlist is empty</p>`;
+    }
+
+    data.forEach((element) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+            <img src="$(card.playlist_art)">
+            <div class="playlist">
+            <h4>${element.playlist_name}</h4>
+            <p1>By: ${element.playlist_author}</p1>
+            <p class="playlist"></p>
+            <button> &#128151; </button>
+            </div>
+            `;
+            
+        parentContainer.appendChild(card)
+
+        
+    });
+
 }
